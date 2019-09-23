2726378897f6e9608b2043b6512f4e0ae2677781
M. Holger <matt@paralleltheory.com>
2019-09-23 11:02:14 -0500

    Replacing corne logo with horde logo

--- keyboards/crkbd/keymaps/holger/rules.mk
+++ keyboards/crkbd/keymaps/holger/rules.mk
@@ -21,7 +21,7 @@ SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 SLEEP_LED_ENABLE = yes    # Breathing sleep LED during USB suspend
 
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/glcdfont.c \
+SRC +=  ./keymaps/holger/horde.c \
         ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
